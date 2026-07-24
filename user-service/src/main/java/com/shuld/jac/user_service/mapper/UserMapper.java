2caaf6f142e3b0f44cf79ad3bbfb8e42522ec28e
mahfaas <stasstasavich@gmail.com>
2026-07-24 17:57:56 +0300

    fix and add user controller

--- user-service/src/main/java/com/shuld/jac/user_service/mapper/UserMapper.java
+++ user-service/src/main/java/com/shuld/jac/user_service/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package com.shuld.jac.user_service.mapper;
 import com.shuld.jac.user_service.dto.UserDto;
 import com.shuld.jac.user_service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toDto(User entity);
 
+    @Mapping(target = "active", ignore = true)
     User toEntity(UserDto dto);
 }
